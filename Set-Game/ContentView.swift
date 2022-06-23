eb4a7a26cef50bb5daffb8ff9a265b2d5a937f21
cabsalvador <salvadorcabrera@csus.edu>
2022-06-22 21:41:04 -0700

    Initial Commit

---
+++ Set-Game/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Set-Game
+//
+//  Created by Sal on 6/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set-Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set-Game
+//
+//  Created by Sal on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
